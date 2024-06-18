02512fa443933a4ab808ea7962c62c17c4ebf64a
Eccleston <matthew.eccleston17@gmail.com>
2024-06-18 21:08:21 +0100

    finished for the day

--- src/views/Entries.tsx
+++ src/views/Entries.tsx
@@ -1,10 +1,11 @@
 import { Typography } from "@mui/material";
 import PostGallery from "../features/PostComponents/PostsGallery";
 
+
 function Entries() {
     return (
         <>
-            <Typography variant='h4' noWrap sx={{ alignSelf: 'center', fontFamily: 'inter', color: 'black', fontWeight: 'bold' }} className='header-main-text'>
+            <Typography variant='h4' noWrap sx={{ alignSelf: 'center', fontFamily: 'inter', color: 'black', fontWeight: 'bold', paddingBottom:'15px'}} className='header-main-text'>
                 Posts
             </Typography>
             <PostGallery />
