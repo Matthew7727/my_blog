a94b65fa693a6c8319d2c7d596ec6bf2746fe844
Eccleston <matthew.eccleston17@gmail.com>
2024-07-27 20:30:30 +0100

    default images

--- src/features/PostComponents/PostCard.tsx
+++ src/features/PostComponents/PostCard.tsx
@@ -1,6 +1,8 @@
 import { Card, CardMedia, CardContent, Typography, CardActions, Button } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import { Posts } from "./pstAPI";
+import blogPostImage from '../../assets/blogPost.png';
+import articleImage from '../../assets/article.png';
 
 interface PostCardProps {
     post: Posts;
@@ -13,9 +15,16 @@ function PostCard({ post }: PostCardProps) {
         navigate(`/entries/${post.id}`);
     };
 
+    // Determine the image to display
+    const imageUrl = post.imageUrls && post.imageUrls.length > 0
+        ? post.imageUrls[0]
+        : post.postType === 'blog'
+            ? blogPostImage
+            : articleImage;
+
     return (
         <Card sx={{ boxShadow: 0, border: '1px solid black', borderRadius: 0 }}>
-            <CardMedia component='img' image={post.imageUrls[0]} title={post.title} />
+            <CardMedia component='img' image={imageUrl} title={post.title} />
             <CardContent>
                 <Typography gutterBottom variant="h5" component='div' sx={{ fontFamily: 'inter', fontWeight: 'bold', textAlign: 'left' }}>
                     {post.title}
